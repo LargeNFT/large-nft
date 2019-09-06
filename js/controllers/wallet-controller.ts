bc29ecc2ceadf08f1d01dd011e068de6594cc369
Patrick Toner <pat.toner2@gmail.com>
2019-09-05 23:48:39 -0400

    Big wallet progress

--- js/controllers/wallet-controller.ts
+++ js/controllers/wallet-controller.ts
@@ -1,6 +1,9 @@
 import { ModelView } from "../model-view";
 import { WalletService } from "../services/wallet-service";
 import { Global } from "../global";
+import { Dom7 } from "framework7";
+
+var $$ = Dom7
 
 class WalletController {
 
@@ -16,18 +19,36 @@ class WalletController {
     }
 
     async showCreateWallet(): Promise<ModelView> {
-        
         return new ModelView(async () => {
-            
-            Global.wallet = await this.walletService.createWallet()
-            Global.wallet = Global.wallet.connect(Global.provider)
-
-            return {
-                mnemonic: Global.wallet.mnemonic
-            }
-
         }, 'pages/wallet/create-wallet.html')
     }
+
+    async createWalletClick(e:Event, component) {
+
+        //@ts-ignore
+        document.getElementById("confirm-password").setCustomValidity("")
+
+        var formData = Global.app.form.convertToData('#create-wallet-form');
+
+        console.log(formData)
+
+        if (formData.password != formData.confirmPassword) {
+            console.log('here')
+            //@ts-ignore
+            document.getElementById("confirm-password").setCustomValidity("Passwords don't match")
+        } else {
+            Global.wallet = await this.walletService.createWallet()
+            Global.wallet = Global.wallet.connect(Global.provider)
+    
+            component.$setState({
+                mnemonic: Global.wallet.mnemonic
+            })
+        }
+
+
+
+    }
+
 
     async showEnterRecovery(): Promise<ModelView> {
         return new ModelView(async () => {

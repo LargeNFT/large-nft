8ac60748fdc2555106d068df743a9fc053587c33
Patrick Toner <pat.toner2@gmail.com>
2019-11-26 23:17:43 -0500

    Test for CI

---
+++ script/dist.sh
@@ -0,0 +1,2 @@
+browserify -s Large -t [ babelify --presets [ @babel/preset-env ] --plugins [ @babel/plugin-proposal-class-properties ] ] ./src/index.ts -p [ tsify --target es2017 --experimentalDecorators ] -o ./www/js/large.js -v; 
+electron-builder -wl
---
+++ script/watch.sh
@@ -0,0 +1,1 @@
+watchify --debug -s Large -t [ babelify --presets [ @babel/preset-env ] --plugins [ @babel/plugin-proposal-class-properties ] ] ./src/index.ts -p [ tsify --target es2017 --experimentalDecorators ]  -o ./www/js/large.js -v

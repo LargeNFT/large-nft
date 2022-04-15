8e4005254b1b30d1a6cc455c75fd9ca19f49ef75
ptoner <ptoner@pm.me>
2022-04-15 09:00:17 -0400

    Publish progress update

--- src/controller/publish-controller.ts
+++ src/controller/publish-controller.ts
@@ -7,6 +7,8 @@ import AdminPublishIndexComponent from '../components/admin/publish/index.f7.htm
 import AdminPublishExportComponent from '../components/admin/publish/export.f7.html'
 import AdminPublishPinataComponent from '../components/admin/publish/pinata.f7.html'
 import AdminPublishForkReaderComponent from '../components/admin/publish/fork-reader.f7.html'
+import AdminPublishPublishReaderComponent from '../components/admin/publish/publish-reader.f7.html'
+import AdminPublishContractComponent from '../components/admin/publish/contract.f7.html'
 
 import { ChannelWebService } from "../service/web/channel-web-service";
 import { PinningService } from "../service/core/pinning-service";
@@ -111,6 +113,50 @@ class PublishController {
 
         }, AdminPublishForkReaderComponent)
     }
+
+
+
+    
+    @routeMap("/admin/publish/publish-reader/:id")
+    async publishReader() : Promise<ModelView> {
+
+        return new ModelView(async (routeTo:RouteTo) => {
+
+            let channelViewModel = await this.channelWebService.get(routeTo.params.id)
+            
+            let gitlab
+
+            try {
+                gitlab = await this.gitlabService.get()
+            } catch(ex) {}
+
+            return {
+                channelViewModel: channelViewModel,
+                gitlab: gitlab
+            }
+
+        }, AdminPublishPublishReaderComponent)
+    }
+
+
+
+    
+    
+    @routeMap("/admin/publish/contract/:id")
+    async contract() : Promise<ModelView> {
+
+        return new ModelView(async (routeTo:RouteTo) => {
+
+            let channelViewModel = await this.channelWebService.get(routeTo.params.id)
+            
+            return {
+                channelViewModel: channelViewModel
+            }
+
+        }, AdminPublishContractComponent)
+    }
+
+
 
 }
 

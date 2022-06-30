5d481aa28e091f2516a6d46f90c31909836ce953
ptoner <ptoner@pm.me>
2022-06-29 20:05:07 -0400

    Test

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -7,7 +7,10 @@ for (let config of configs) {
     //@ts-ignore
     mainConfigs.push(merge(config, {
         //@ts-ignore
-        mode: 'production'
+        //@ts-ignore
+        mode: 'development',
+        //@ts-ignore
+        devtool: 'source-map',
     }))
 }
 
